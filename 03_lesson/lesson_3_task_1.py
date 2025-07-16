80f4378b3ee8386b7c848ced7711f2dfb0f2c714
Eugeneiya <eugeneiyab@gmail.com>
2025-07-16 19:42:24 +0300

    Add files via upload

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,8 @@
+from user import User
+
+my_user = User("Alex", "Bolton")
+
+# Вызываем методы и выводим результаты
+print(my_user.get_name())
+print(my_user.get_surname())
+print(my_user.get_user_info())
